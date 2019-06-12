d70cc61b5024a63ec658d1671ba8513767e4de49
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-11 22:57:25 -0500

    added my own objects and created my own function

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ var mark = new Person('Mark', 1948, 'retired');
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+
+
+
+var Puppy = function (name, owner, color) {
+    this.name = name;
+    this.owner = owner;
+    this.color = color;
+}
+
+Puppy.prototype.belongs = function() {
+    console.log(this.name + ' belongs to ' + this.owner + ' and he is '+ this.color);
+};
+
+var chance = new Puppy('Chance', 'Julie', 'black');
+
+var chloe = new Puppy ('Chloe', 'Bacchus', 'white');
+
+var miles = new Puppy ('Miles', 'Bacchus', 'brown');
+
+chance.belongs();
+chloe.belongs();
+miles.belongs();
+
