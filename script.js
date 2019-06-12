6cf2188d1306ad3f17af579aca4e10b76698c9ce
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-11 20:53:07 -0500

    updating and learning abotu objects and functions

---
+++ script.js
@@ -0,0 +1,27 @@
+//function construction 
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+//using a function construction is like a pattern for a blueprint alwaus with a capital
+
+var Person  = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job; 
+    }
+
+
+Person.prototype.calculateAge = function () {
+    console.log(2016 - this.yearOfBirth);
+};
+var john = new Person('john', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
