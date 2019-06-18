5add97ede4155f521aa347aa41b749b08e82163c
Bacchus <47571329+Ljimenez01@users.noreply.github.com>
2019-06-17 20:41:23 -0500

    inheritance and prototypes

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
+     
+}
+//inheritance - objects have access to this function because it is in their
+//prototype
+Person.prototype.calculateAge = function (){
+    console.log(2019 - this.yearOfBirth)
 }
 
 var john = new Person('John', 1990, 'teacher');
+
+var jane = new Person('Jennifer', 1977, 'Make Up Artist');
+
+var bacchus = new Person ('Bacchus', 1977, 'glass engraver');
+
+john.calculateAge();
+jane.calculateAge();
+bacchus.calculateAge();
