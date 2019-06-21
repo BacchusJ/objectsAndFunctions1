c790a1d07cf45414dca746d63ead7b5ddf0fb159
Bacchus <47571329+Ljimenez01@users.noreply.github.com>
2019-06-21 00:07:52 -0500

    challenge - questions
    display questions on the in the console and ask them through a prompt box

--- script.js
+++ script.js
@@ -222,6 +222,8 @@ interviewQuestion('xfinity')('Danny');
 
 //bind call and apply methods 
 
+/*
+
 var john = {
     name: 'John',
     age: 26,
@@ -285,9 +287,65 @@ var kahFriendly = john.presentation.bind(kah, 'friendly', 'morning');
 // kahFriendly('morning');
 kahFriendly("evening");
 
+*/
+//IIFE - this prevents the code to interfere with another developer's code. In case they are using the same name for a variable
+(function () {
+//create a function constructor! 
+
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+
+}
+//this displays the questions 
+Question.prototype.displayQuestion = function () {
+console.log(this.question);
+//this will loop through the answers 
+for (var i = 0; i < this.answers.length; i++ ) {
+    console.log(i + ': ' + this.answers[i]);
+}
+}
+
+//to check if we got the correct answer 
+
+Question.prototype.checkAnswer = function(ans){
+if (ans === this.correct) {
+    console.log('Correct Answer')
+} else {
+    console.log('Try Again')
+}
+}
 
 
 
 
+var q1 = new Question('Is JavaScript the coolest programing language in the world?',
+
+                    ['yes', 'no'],
+
+                    0);
 
 
+var q2 = new Question('What is the name of this course\'s instructor?',
+                    ['John',
+                     'Michael',
+                     'Jonas'],
+                     2);
+
+var q3 = new Question('What does best describe coding?', 
+                    ['Boring',
+                     'Hard',
+                     'Fun',
+                     'Tedious'],
+                     2);         
+                     
+var questions = [q1, q2, q3];
+//multiply - this will run through the array 
+var n = Math.floor(Math.random() * questions.length)
+
+questions[n].displayQuestion();
+//use parseInt to make sure it turns the answer into a string 
+var answer = parseInt(prompt('Please select the correct answer.'));
+questions[n].checkAnswer(answer);
+}) ();
