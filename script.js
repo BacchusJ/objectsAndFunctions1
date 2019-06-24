57ba5f8f6c5c9e2806deb3c0d6251f79fd9c5271
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-23 20:53:02 -0500

    completed

--- script.js
+++ script.js
@@ -354,11 +354,10 @@ questions[n].checkAnswer(answer);
 
 */
 
-
 //IIFE - this prevents the code to interfere with another developer's code. In case they are using the same name for a variable
 (function () {
-    //create a function constructor! 
-    
+
+
     function Question(question, answers, correct) {
         this.question = question;
         this.answers = answers;
@@ -376,15 +375,24 @@ questions[n].checkAnswer(answer);
     
     //to check if we got the correct answer 
     
-    Question.prototype.checkAnswer = function(ans){
+    Question.prototype.checkAnswer = function(ans, callback){
+        var sc;
     if (ans === this.correct) {
-        console.log('Correct Answer')
+        console.log('Correct Answer');
+        sc = callback(true);
     } else {
-        console.log('Try Again')
-    }
+        console.log('Try Again');
+        sc = callback(false);
+        }
+
+        this.displayScore(sc);
     }
     
-    
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('----------------------------');
+
+    }
     
     
     var q1 = new Question('Is JavaScript the coolest programing language in the world?',
@@ -405,33 +413,47 @@ questions[n].checkAnswer(answer);
                          'Hard',
                          'Fun',
                          'Tedious'],
-                         2);   
+                         2);         
      
-    var questions = [q1, q2, q3]; 
-    
-    
+    var questions = [q1, q2, q3];  
+    //this functions will keep the score. 
+    //notice we are using ++ for the score to increment by 1 while it keeps count
+    function score() {
+        var sc = 0;
+        return function (correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+    }
+ }
+    var keepScore = score();
 
     function nextQuestion () {
-        
+
+       
         //multiply - this will run through the array 
         var n = Math.floor(Math.random() * questions.length)
         
         questions[n].displayQuestion();
         //use parseInt to make sure it turns the answer into a string 
         var answer = prompt('Please select the correct answer.');
-      
-        if(answer !== 'exit') {
-            questions[n].checkAnswer(parseInt(answer));
+        
+        //this will allow the game to quit if the user types "exit"
+        // we move the parseInt because "exit" is not an integer
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
             nextQuestion();
         }
 
         
+        
+    };
 
-    }                     
-                         
     nextQuestion();
 
-    }) ();
 
 
+})()
+    
     
